29a774b7471d34c2726e75ca28f1332ed26b1a21
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-15 13:22:18 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  let result = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  return result; 
 }
 
 function renderBooks(books) {
